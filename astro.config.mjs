87d90bae5c6aef4e3d13d72491b7c9cd1565c801
Óscar Bustos <oscarbustosgarrido@gmail.com>
2025-02-17 10:51:52 +0100

    feat: performance improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,48 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import path from 'path';
 import react from '@astrojs/react';
+import compressor from 'astro-compressor';
 
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover'
+  },
   trailingSlash: 'always',
   site: 'https://oscarbustos.dev',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    compressor({
+      fileType: ['html', 'css', 'js', 'svg', 'xml']
+    })
+  ],
   vite: {
-      resolve: {
-          alias: {
-              '@': path.resolve('./src')
-          }
+    build: {
+      cssMinify: true,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true
+        }
       }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    },
+    ssr: {
+      noExternal: ['@fontsource-variable/*']
+    }
   },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    },
+    domains: ['oscarbustos.dev']
+  }
 });
